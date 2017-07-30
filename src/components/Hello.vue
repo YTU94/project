7b1d9d313724b941be3c548746537c5e2e9a0c70
ytu94 <453980450@453980450@qq.com>
2017-07-30 19:56:23 +0800

    7.30

---
+++ src/components/Hello.vue
@@ -0,0 +1,249 @@
+<template>
+  <div class="home">
+    <transition name="slide-fade-left" mode="out-in">
+      <div class="pic1" v-if="show"><img :src="img1" alt="交投" width="100%" height="100%"></div>
+    </transition>
+    <transition name="run">
+      <div class="pic2 run-enter-active"><img :src="img2" alt="耳机" width="100%" height="100%"></div>
+    </transition>
+    <transition name="scale">
+      <div class="pic3" v-if="show"><img :src="img3" alt="三根草" width="100%" height="100%"></div>
+    </transition>
+    <transition name="slide-fade-right">
+      <div class="pic4" v-if="show"><img :src="img4" alt="惠青年" width="100%" height="100%"></div>
+    </transition>
+    <transition name="slide-fade-bottom">
+      <div class="pic5" v-if="show"><img :src="img5" alt="盛夏嗨翻天" width="100%" height="100%"></div>
+    </transition>
+    <div class="pic6 run-lb-enter-active"><img :src="img6" alt="长颈鹿" width="100%" height="100%"></div>
+    <div class="pic7"><img :src="img7" alt="网易严选" class="" width="100%" height="100%"></div>
+    <div class="pic8">
+      <img :src="img8" alt="" width="100%" height="100%">
+      <router-link to="/pageOne" class="scale-enter-active"></router-link>
+    </div>
+    <div class="pic9">
+      <img :src="img9" alt="" width="100%" height="100%">
+      <router-link to="/pageTwo" class="scale-enter-active"></router-link>      
+    </div>
+  </div>
+</template>
+
+<script>
+import homeBg from '../assets/image/home/home_bg.png'
+import img1 from '../assets/image/home/img1.png'
+import img2 from '../assets/image/home/img2.png'
+import img3 from '../assets/image/home/img3.png'
+import img4 from '../assets/image/home/img4.png'
+import img5 from '../assets/image/home/img5.png'
+import img6 from '../assets/image/home/img6.png'
+import img7 from '../assets/image/home/img7.png'
+import img8 from '../assets/image/home/img8.png'
+import img9 from '../assets/image/home/img9.png'
+import kuang8 from '../assets/image/home/kuang_8.png'
+import kuang9 from '../assets/image/home/kuang_9.png'
+import homeBtn from '../assets/image/home/home_btn.png'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      show: false,
+      homeBg: homeBg,
+      img1: img1,
+      img2: img2,
+      img3: img3,
+      img4: img4,
+      img5: img5,
+      img6: img6,
+      img7: img7,
+      img8: img8,
+      img9: img9,
+      kuang8: kuang8,
+      kuang9: kuang9,
+      homeBtn: homeBtn,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted: function () {
+    this.show = true
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+/*
+* 动画
+*/
+.slide-fade-left-enter-active, .slide-fade-right-enter-active, .scale-enter-active, .slide-fade-bottom-enter-active{
+  transition: all .8s cubic-bezier(1.0, 0.8, 0.8, 1.0);
+}
+.run-enter-active {
+  animation: rock .7s linear .9s infinite alternate;
+}
+.run-lb-enter-active{
+  transform-origin: left bottom;  
+  animation: rock .9s linear .9s infinite alternate;
+}
+.scale-enter-active {
+  animation: scale 1.0s linear .9s infinite alternate;
+}
+.slide-fade-left-enter {
+  transform: translateX(-50px);
+  opacity: 0;
+}
+.slide-fade-right-enter {
+  transform: translateX(50px);
+  opacity: 0;
+}
+.scale-enter{
+  transform: scale(0.1);
+  opacity: 0;
+}
+.slide-fade-bottom-enter{
+  transform: translateY(10px);
+  opacity: 0;
+}
+// 摇摆动画
+.run-enter{
+}
+@keyframes rock {
+  0% {
+    transform: rotate(10deg)
+  }
+  50% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(-10deg)
+  }
+}
+@keyframes scale {
+  0% {
+    transform: scale(1) rotate(0deg) translateX(0px)
+  }
+  5% {
+    transform: scale(1) rotate(0deg) translateX(2px)    
+  }
+  10% {
+    transform: scale(1) rotate(0deg) translateX(-2px)
+  }
+  15%{
+    transform: scale(1) rotate(0deg) translateX(2px)
+  }
+  20%{
+    transform: scale(1) rotate(0deg) translateX(-2px)    
+  }
+  25% {
+    transform: scale(1) rotate(0deg) translateX(0px)
+  }
+  35% {
+    transform: scale(1) rotate(-0deg) translateX(-2px)
+  }
+  50% {
+    transform: scale(1) rotate(0deg) translateX(0px)
+  }
+  100% {
+    transform: scale(1)
+  }
+}
+.home{
+  width:100%;
+  height: 23.626667rem;
+  overflow-x: hidden;
+  background: url('../assets/image/home/home_bg.png') no-repeat center;
+  background-size: 100% 100%;
+  .pic1, .pic2, .pic3, .pic4, .pic5, .pic6, .pic7, .pic8, .pic9{
+    position: absolute;
+    overflow: hide;
+    display: inline-block;
+  }
+  .pic1{
+    top: 1.6rem;
+    left:  .746667rem;
+    width: 2.133333rem;
+    height: 1.146667rem;
+  }
+  .pic2{
+    top: 2.7rem;
+    left: .746667rem;
+    width: 1.333333rem;
+    height: 1.493333rem;
+  }
+  .pic3{
+    top: 2rem;
+    left: 4rem;
+    width: 1.2rem;
+    height: .506667rem;
+  }
+  .pic4{
+    top: 2.25rem;
+    left: 2.713333rem;
+    width: 5.973333rem;
+    height: 1.73333rem;
+  }
+  .pic5{
+    top: 3.653333rem;
+    left: 1.066667rem;
+    width: 7.6rem;
+    height: 2.133333rem;
+  }
+  .pic6{
+    top: 5.706667rem;
+    left: -.5rem;
+    width: 1.706667rem;
+    height: 3.493333rem;
+    transform: rotateX(45deg);
+  }
+  .pic7{
+    top: 6.213333rem;
+    left: 1.973333rem;
+    width: 6.933333rem;
+    height: .88rem;
+  }
+  .pic8{
+    top: 11.60rem;
+    left: .666667rem;
+    width: 8.4rem;
+    height: 4.133333rem;
+    background: url('../assets/image/home/kuang_8.png') no-repeat center;
+    background-size: 100% 100%;
+    box-sizing: border-box;
+    padding-top: 1.066667rem;
+    img{
+      width: 7.36rem;
+      height: 1.653333rem;
+    }
+    a{
+      display: inline-block;
+      width: 2.293333rem;
+      height: .64rem;
+      background: url('../assets/image/home/home_btn.png') no-repeat center;
+      background-size: 100% 100%;
+    } 
+  }
+  .pic9{
+    top: 16.333333rem;
+    left: .666667rem;
+    width: 8.48rem;
+    height: 4.72rem;
+    background: url('../assets/image/home/kuang_9.png') no-repeat center;
+    background-size: 100% 100%;
+    box-sizing: border-box;
+    padding-top: .7rem;
+    img{
+      width: 6.56rem;
+      height: 2.426667rem;
+    }
+    a{
+      display: inline-block;
+      width: 2.293333rem;
+      height: .64rem;
+      background: url('../assets/image/home/home_btn.png') no-repeat center;
+      background-size: 100% 100%;
+    }     
+  }
+  a{
+    text-decoration: none;
+  }
+}
+</style>
---
+++ src/components/pageOne.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="home">
+    <router-link to="/"><img :src="infoBack" alt="" class="back"></router-link>
+    <router-link to="/"><img :src="infoClose" alt="" class="close"></router-link>
+    <div class="use">
+      <img :src="infoOneT" alt="" class="title" width="">
+      <img :src="useInput" alt="" class="input">
+      <a href="http://u.163.com/aosoutdtdt1"></a>
+    </div>
+    <div class="useInfo">
+      <img :src="infoOne" alt="" width="100%" height="100%">
+    </div>
+  </div>
+</template>
+
+<script>
+import infoBack from '../assets/image/info_back.png'
+import infoClose from '../assets/image/info_close.png'
+import infoOne from '../assets/image/info_one.png'
+import infoOneT from '../assets/image/info_one_t.png'
+import useInput from '../assets/image/use_input.png'
+export default {
+  data () {
+    return {
+      infoBack: infoBack,
+      infoClose: infoClose,
+      infoOne: infoOne,
+      infoOneT: infoOneT,
+      useInput: useInput
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home{
+  width: 100%;
+  height: 16.586667rem;
+  background: url('../assets/image/info_bg.png') no-repeat center;
+  background-size: 100% 100%;
+  .back, .close{
+    position: absolute;
+    top: .36rem;
+    width: 1.706667rem;
+    height: .746667rem;
+  }
+  .back{
+    left: .453333rem;
+  }
+  .close{
+    right: .453333rem;
+  }
+  .use{
+    background: url('../assets/image/use_bg.png') no-repeat center;
+    background-size: 100% 100%;
+    position: absolute;
+    top: 1.84rem;
+    left: .8rem;
+    width: 8.4rem;
+    height: 4rem;
+    box-sizing: border-box;
+    padding-top: .9rem;
+    .title{
+      display: inline-block;
+      width: 4.4rem;
+      height: .72rem;
+    }
+    .input{
+      display: inline-block;
+      width: 6.533333rem;
+      height: 1.013333rem;
+    }
+    a{
+      display: inline-block;
+      width: 2.32rem;
+      height: .64rem;
+      background: url('../assets/image/use_btn.png') no-repeat center;
+      background-size: 100% 100%;
+    }
+  }
+  .useInfo{
+    position: absolute;
+    top: 8.2rem;
+    left: 1.0rem;
+    width: 8.0rem;
+    height: 6.613333rem;
+  }
+}
+</style>
